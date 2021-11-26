17daa95c1247771d1e356018d68ede720b887b29
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-26 15:25:51 +0530

    import dns and initialize

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+import dns from "dns";
+
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request) {
-  return new Response("Hello worker!", {
+  let reqUrl = new URL(request.url);
+  let url = reqUrl.searchParams.get("url");
+
+  const options = {
+    family: 6,
+    hints: dns.ADDRCONFIG | dns.V4MAPPED,
+  };
+  let result = dns.lookup(url, options, (err, address, family) => {
+    return `address: ${address} family: IPv${family}`;
+  });
+
+  return new Response(result, {
     headers: { "content-type": "text/plain" },
   });
 }
