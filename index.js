916e44343c534b3f861d1c4058e5182c3f4ba38a
Caleb Joshua <43238940+calebjoshuapaul@users.noreply.github.com>
2021-11-26 16:02:02 +0530

    Update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as dns from "dns";
+// import * as dns from "dns";
 
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
@@ -8,13 +8,13 @@ async function handleRequest(request) {
   let reqUrl = new URL(request.url);
   let url = reqUrl.searchParams.get("url");
 
-  const options = {
-    family: 6,
-    hints: dns.ADDRCONFIG | dns.V4MAPPED,
-  };
-  let result = dns.lookup(url, options, (err, address, family) => {
-    return `address: ${address} family: IPv${family}`;
-  });
+  // const options = {
+  //   family: 6,
+  //   hints: dns.ADDRCONFIG | dns.V4MAPPED,
+  // };
+  // let result = dns.lookup(url, options, (err, address, family) => {
+  //   return `address: ${address} family: IPv${family}`;
+  // });
 
   return new Response("result", {
     headers: { "content-type": "text/plain" },
